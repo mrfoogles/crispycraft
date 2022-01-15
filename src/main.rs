b49a547e9ac3e3203b76116cb3b873a5ef4a5d2c
mrfoogles <48422925+mrfoogles@users.noreply.github.com>
2022-01-14 16:12:05 -0800

    Window setup for update/render; winit_input_helper

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use winit::{
+    window::{Window,WindowBuilder},
+    event::*,
+    event_loop::{EventLoop, ControlFlow}
+};
+use winit_input_helper::WinitInputHelper;
+
 fn main() {
-    println!("Hello, world!");
+    let evloop = EventLoop::new();
+
+    let window = WindowBuilder::new()
+        .build(&evloop).unwrap();
+
+    let mut input = WinitInputHelper::new();
+    
+    evloop.run(move |main_event, _, control_flow| {
+        if input.update(&main_event) {
+            // Update
+        }
+
+        match main_event {
+            Event::WindowEvent {
+                window_id,
+                event: window_event
+            } if window_id == window.id() => {
+                match window_event {
+                    WindowEvent::CloseRequested => { *control_flow = ControlFlow::Exit },
+                    _ => {}
+                }
+            },
+            Event::RedrawRequested(window_id) if window_id == window.id() => {
+                // Render
+            },
+            _ => {}
+        }
+    });
 }
