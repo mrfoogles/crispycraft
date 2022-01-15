e29733a5b1219d7c81d7fdff60de22786b306933
mrfoogles <48422925+mrfoogles@users.noreply.github.com>
2022-01-14 20:16:40 -0800

    Render a triangle
    I added a State struct, mostly as in the WebGPU tutorial, but I also tried to make it less verbose.
    
    CPUMesh and GPUMesh are a way of uploading a mesh to the gpu.
    Ex: let gpu_mesh = cpu_mesh.upload(&device);

--- src/game.rs
+++ src/game.rs
@@ -2,34 +2,79 @@
 use wgpu::*;
 use wgpu::util::{BufferInitDescriptor, DeviceExt};
 
+use winit::window::Window;
+
 #[repr(C)]
+#[derive(bytemuck::Pod, bytemuck::Zeroable, Clone, Copy)]
 pub struct Vertex {
     pos: [f32; 3]
+}
+impl Vertex {
+    fn desc<'a>() -> VertexBufferLayout<'a> {
+        VertexBufferLayout {
+            array_stride: std::mem::size_of::<Self>() as BufferAddress,
+            step_mode: VertexStepMode::Vertex,
+            attributes: &[
+            VertexAttribute {
+                offset: 0,
+                shader_location: 0,
+                format: VertexFormat::Float32x3,
+            },
+            VertexAttribute {
+                offset: std::mem::size_of::<[f32; 3]>() as BufferAddress,
+                shader_location: 1,
+                format: VertexFormat::Float32x3,
+            }
+            ]
+        }
+    }
 }
 
 pub type Index = u16;
 const INDEX_FORMAT: IndexFormat = IndexFormat::Uint16;
+const VERT_ENTRY_POINT: &str = "vs_main";
+const FRAG_ENTRY_POINT: &str = "fs_main";
 
 pub struct CPUMesh {
     verts: Vec<Vertex>,
     indxs: Vec<Index>
 }
+impl CPUMesh {
+    fn upload(&self, device: &Device) -> GPUMesh {
+        let vertbuf = device.create_buffer_init(
+            &wgpu::util::BufferInitDescriptor {
+                label: None,
+                contents: bytemuck::cast_slice(&self.verts),
+                usage: BufferUsages::VERTEX | BufferUsages::COPY_DST,
+            }
+        );
+        let indxbuf = device.create_buffer_init(
+            &wgpu::util::BufferInitDescriptor {
+                label: None,
+                contents: bytemuck::cast_slice(&self.indxs),
+                usage: BufferUsages::INDEX | BufferUsages::COPY_DST,
+            }
+        );
+
+        return GPUMesh {
+            vertbuf,
+            num_verts: self.verts.len() as u32,
+            indxbuf,
+            num_indxs: self.indxs.len() as u32
+        }
+    }
+    fn update_gpu_mesh(&self, gpu_mesh: &mut GPUMesh, queue: &Queue) {
+        queue.write_buffer(&gpu_mesh.vertbuf, 0, bytemuck::cast_slice(&self.verts));
+        queue.write_buffer(&gpu_mesh.indxbuf, 0, bytemuck::cast_slice(&self.indxs));
+    }
+}
+
 pub struct GPUMesh {
     vertbuf: Buffer,
     num_verts: u32,
     indxbuf: Buffer,
     num_indxs: u32
 }
-impl GPUMesh {
-    fn from_cpu(device: &Device, cpu_mesh: &CPUMesh) -> Self {
-        todo!()
-    }
-    fn update_from_cpu(&mut self, queue: &Queue, cpu_mesh: &CPUMesh) {
-        todo!()
-    }
-}
-// self.gpu_mesh = GPUMesh::from_cpu(mesh::mesh_from_chunk(&chunk, &mut stage_buffer))
-// self.gpu_mesh.update_from_cpu(mesh::mesh_from_chunk(&quads_buffer))
 
 
 fn draw_mesh<'a,'b: 'a>(pass: &mut RenderPass<'a>, mesh: &'b GPUMesh, instances: u32) {
@@ -39,15 +84,122 @@ fn draw_mesh<'a,'b: 'a>(pass: &mut RenderPass<'a>, mesh: &'b GPUMesh, instances:
     pass.draw_indexed(0..mesh.num_indxs, 0, 0..instances);
 }
 
-struct State {
+pub struct State {
     device: Device,
     queue: Queue,
     surface: Surface,
+    config: SurfaceConfiguration,
+    pub size: winit::dpi::PhysicalSize<u32>,
 
     pipeline: RenderPipeline,
     gpu_mesh: GPUMesh
 }
 impl State {
+    pub async fn new(window: &Window) -> Self {
+        let size = window.inner_size();
+
+        let instance = wgpu::Instance::new(wgpu::Backends::all());
+        let surface = unsafe { instance.create_surface(window) };
+        let adapter = instance.request_adapter(
+            &wgpu::RequestAdapterOptions {
+                power_preference: wgpu::PowerPreference::default(),
+                compatible_surface: Some(&surface),
+                force_fallback_adapter: false,
+            },
+        ).await.unwrap();
+
+        let (device, queue) = adapter.request_device(
+            &wgpu::DeviceDescriptor {
+                features: wgpu::Features::empty(),
+                limits: wgpu::Limits::default(),
+                label: None,
+            },
+            None, // Trace path
+        ).await.unwrap();
+
+        let config = wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            format: surface.get_preferred_format(&adapter).unwrap(),
+            width: size.width,
+            height: size.height,
+            present_mode: wgpu::PresentMode::Fifo,
+        };
+        surface.configure(&device, &config);
+
+        let layout = device.create_pipeline_layout(&PipelineLayoutDescriptor {
+            label: None,
+            bind_group_layouts: &[],
+            push_constant_ranges: &[]
+        });
+
+        let shader = device.create_shader_module(&ShaderModuleDescriptor {
+            label: Some("Shader"),
+            source: ShaderSource::Wgsl(include_str!("./shader.wgsl").into()),
+        });
+
+        let pipeline = device.create_render_pipeline(&RenderPipelineDescriptor {
+            label: None,
+            layout: Some(&layout),
+            vertex: VertexState {
+                module: &shader,
+                entry_point: VERT_ENTRY_POINT,
+                buffers: &[Vertex::desc()]
+            },
+            fragment: Some(FragmentState {
+                module: &shader,
+                entry_point: &FRAG_ENTRY_POINT,
+                targets: &[ColorTargetState {
+                    format: config.format,
+                    blend: Some(BlendState::REPLACE),
+                    write_mask: ColorWrites::ALL
+                }]
+            }),
+            primitive: PrimitiveState {
+                front_face: FrontFace::Ccw,
+                cull_mode: None,//Some(Face::Back),
+                ..PrimitiveState::default()
+            },
+            depth_stencil: None,
+            multisample: MultisampleState::default(),
+            multiview: None
+        });
+
+        let gpu_mesh = CPUMesh {
+            verts: vec![
+                Vertex {
+                    pos: [0.0,0.5,0.0]
+                },
+                Vertex {
+                    pos: [-0.5,-0.5,0.0]
+                },
+                Vertex {
+                    pos: [0.5,-0.5,0.0]
+                },
+            ],
+            indxs: vec![0,1,2]
+        }.upload(&device);
+
+        Self {
+            surface,
+            device,
+            queue,
+            config,
+            size,
+
+            pipeline,
+            gpu_mesh
+        }
+    }
+
+    pub fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
+        if new_size.width > 0 && new_size.height > 0 {
+            self.size = new_size;
+            self.config.width = new_size.width;
+            self.config.height = new_size.height;
+            self.surface.configure(&self.device, &self.config);
+        }
+    }
+
     pub fn render(&self) -> Result<(), SurfaceError> {
         let output = self.surface.get_current_texture()?;
         let view = output.texture.create_view(&wgpu::TextureViewDescriptor::default());
@@ -73,6 +225,7 @@ impl State {
                 }],
                 depth_stencil_attachment: None // TODO!!!
             });
+
             pass.set_pipeline(&self.pipeline);
             draw_mesh(&mut pass, &self.gpu_mesh, 1);
         };
--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,8 @@ fn main() {
         .build(&evloop).unwrap();
 
     let mut input = WinitInputHelper::new();
-    
+    let mut state = pollster::block_on(game::State::new(&window));
+
     evloop.run(move |main_event, _, control_flow| {
         if input.update(&main_event) {
             // Update
@@ -31,12 +32,22 @@ fn main() {
                     WindowEvent::CloseRequested => { *control_flow = ControlFlow::Exit },
                     WindowEvent::Resized(new_size) => {
                         // Resize
+                        state.resize(new_size);
                     },
                     _ => {}
                 }
             },
             Event::RedrawRequested(window_id) if window_id == window.id() => {
                 // Render
+                match state.render() {
+                    Ok(_) => {}
+                    // Reconfigure the surface if lost
+                    Err(wgpu::SurfaceError::Lost) => state.resize(state.size),
+                    // The system is out of memory, we should probably quit
+                    Err(wgpu::SurfaceError::OutOfMemory) => *control_flow = ControlFlow::Exit,
+                    // All other errors (Outdated, Timeout) should be resolved by the next frame
+                    Err(e) => eprintln!("{:?}", e),
+                }
             },
             _ => {}
         }
